b031496f748451a399642f3af1e7aa7eb3a3ee1d
Ankitha-GS <ankithags44@gmail.com>
2026-04-13 17:59:09 +0530

    Create palindrome_array.py

---
+++ Arrays/palindrome_array.py
@@ -0,0 +1,20 @@
+
+Problem: Check if Array is Palindrome
+Time Complexity: O(n)
+Space Complexity: O(1)
+Code:
+
+def is_palindrome(arr):
+    left, right = 0, len(arr) - 1
+    
+    while left < right:
+        if arr[left] != arr[right]:
+            return False
+        left += 1
+        right -= 1
+    
+    return True
+
+
+print(is_palindrome([1, 2, 3, 2, 1]))
+
