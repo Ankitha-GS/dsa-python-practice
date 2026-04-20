5db38e7f789eed83780e9ee444719a0a1514d18b
Ankitha-GS <ankithags44@gmail.com>
2026-04-20 17:35:58 +0530

    Maximum_Average_Subarray _I.py

---
+++ Arrays/Maximum_Average_Subarray _I.py
@@ -0,0 +1,17 @@
+
+Problem: Maximum Average Subarray I (LeetCode 643)
+Approach: Fixed Sliding Window
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+
+class Solution:
+    def findMaxAverage(self, nums, k):
+        window_sum = sum(nums[:k])
+        max_sum = window_sum
+        
+        for i in range(k, len(nums)):
+            window_sum += nums[i] - nums[i - k]
+            max_sum = max(max_sum, window_sum)
+        
+        return max_sum / k
